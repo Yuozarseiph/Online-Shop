3de3a3a86cf6a917f9b6aaae75e3654fa87ab6c4
Yousef Shaker <150653746+Yuozarseiph@users.noreply.github.com>
2025-10-20 23:01:40 +0330

    Update BannerSlider.jsxoptimized

--- src/components/BannerSlider.jsx
+++ src/components/BannerSlider.jsx
@@ -1,4 +1,3 @@
-// src/components/BannerSlider.js
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
